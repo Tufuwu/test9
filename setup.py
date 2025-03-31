8ecdd5c6f386a5ac475045d031cec294da663eff
wu <wu@.com>
2025-04-01 06:42:19 +0800

    pycasbin/django-orm-adapter/action

--- setup.py
+++ setup.py
@@ -1,58 +1,30 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import setuptools
 
-import sys
-import codecs
-from setuptools import setup, find_packages
+desc_file = "README.md"
 
+with open(desc_file, "r") as fh:
+    long_description = fh.read()
 
-setup(
-    name='sphinxcontrib-images',
-    version='0.9.2',
-    url='https://github.com/sphinx-contrib/images',
-    download_url='https://pypi.python.org/pypi/sphinxcontrib-images',
-    project_urls={
-        'Bug Tracker': 'https://github.com/sphinx-contrib/images/issues',
-        'Documentation': 'https://sphinxcontrib-images.readthedocs.io/',
-    },
-    license='Apache 2',
-    author=u'Tomasz Czyż',
-    author_email='tomasz.czyz@gmail.com',
-    description='Sphinx extension for thumbnails',
-    long_description=codecs.open('README.rst', encoding="utf8").read(),
-    zip_safe=False,
+setuptools.setup(
+    name="casbin-django-orm-adapter",
+    author="Yang Luo",
+    author_email="hsluoyz@qq.com",
+    description="Django's ORM adapter for PyCasbin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pycasbin/django-orm-adapter",
+    keywords=["casbin", "adapter", "storage-driver", "django", "orm", "django-orm", "access-control", "authorization"],
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=["casbin", "django"],
+    python_requires=">=3.7",
+    license="Apache 2.0",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Documentation',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
-    entry_points={
-        'console_scripts':[
-            'sphinxcontrib-images=sphinxcontrib.images:main',
-        ],
-        'sphinxcontrib.images.backend':[
-            'LightBox2 = sphinxcontrib_images_lightbox2:LightBox2',
-            'FakeBackend = sphinxcontrib_images_lightbox2:LightBox2',
-        ]
-    },
-    platforms='any',
-    packages=find_packages(),
-    include_package_data=True,
-    setup_requires=['wheel'],
-    install_requires=['sphinx>=1.8.5,<2.0;python_version<"3.0"',
-                      'sphinx>=2.0;python_version>="3.0"',
-                      'requests>2.2,<3'],
-    namespace_packages=['sphinxcontrib'],
+    data_files=[desc_file],
 )
