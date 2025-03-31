d149bc93d7268fecbcc52aa7adc887a71713f6a1
wu <wu@.com>
2025-03-31 23:57:11 +0800

    opencivicdata/python-opencivicdata/action

--- setup.py
+++ setup.py
@@ -1,55 +1,35 @@
-#!/usr/bin/python
-import os
-import re
-import subprocess
-from setuptools import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
+install_requires = ["Django>=2.2", "psycopg2-binary"]
 
-
-def get_version():
-    """Get the version from git tags.
-
-    Version is determined by the latest git tag, and will be the tag name without the leading 'v'.
-
-    Returns:
-        str: The version number.
-    """
-    try:
-        # Get the latest git tag
-        version = subprocess.check_output(
-            ["git", "describe", "--tags", "--abbrev=0"], encoding="utf-8"
-        ).strip()
-        version = re.sub("^v", "", version)
-        return version
-
-    except subprocess.CalledProcessError:
-        return "0.0.0"
+extras_require = {
+    "dev": ["pytest>=3.6", "pytest-cov", "pytest-django", "coveralls", "flake8"]
+}
 
 
 setup(
-    name="anybadge",
-    description="Simple, flexible badge generator for project badges.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=get_version(),
-    author="Jon Grace-Cox",
-    author_email="30441316+jongracecox@users.noreply.github.com",
-    packages=["anybadge", "anybadge.templates", "anybadge.server"],
-    py_modules=["anybadge_server"],
-    setup_requires=["setuptools", "wheel"],
-    tests_require=[],
-    install_requires=["packaging"],
-    package_data={"anybadge": ["templates/*.svg"]},
-    options={"bdist_wheel": {"universal": False}},
-    python_requires=">=3.7",
-    url="https://github.com/jongracecox/anybadge",
-    entry_points={
-        "console_scripts": [
-            "anybadge=anybadge.cli:main",
-            "anybadge-server=anybadge.server.cli:main",
-        ],
-    },
-    classifiers=["License :: OSI Approved :: MIT License"],
+    name="opencivicdata",
+    version="3.2.0",
+    author="James Turk",
+    author_email="james@openstates.org",
+    license="BSD",
+    description="python opencivicdata library",
+    long_description="",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
