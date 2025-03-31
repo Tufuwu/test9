965f88dbac6745a877451fae82e42b183957945f
wu <wu@.com>
2025-04-01 01:13:05 +0800

    enthought/codetools/action

--- src/matching/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Top-level imports only. """
-
-from .game import BaseGame
-from .matching import Matching
-from .player import Player
-from .version import __version__
--- src/matching/players/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Make the player classes more accessible. """
-
-from .hospital import Hospital
-from .project import Project
-from .supervisor import Supervisor
