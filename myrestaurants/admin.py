69604bb5103f62390f9ab3bba5d018a853f0e468
wu <wu@.com>
2025-04-01 00:04:29 +0800

    modscripps/mixsea/action

--- myrestaurants/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from myrestaurants.models import Restaurant, Dish, RestaurantReview
-
-admin.site.register(Restaurant)
-admin.site.register(Dish)
-admin.site.register(RestaurantReview)
