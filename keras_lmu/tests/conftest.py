5be170a8a74fea7540696ba2020630a773126c91
wu <wu@.com>
2025-04-01 02:37:41 +0800

    zaproxy/zap-api-python/action

--- keras_lmu/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-docstring
-
-import tensorflow as tf
-from packaging import version
-
-
-def pytest_configure(config):
-    if version.parse(tf.__version__) >= version.parse("2.7.0"):
-        tf.debugging.disable_traceback_filtering()
