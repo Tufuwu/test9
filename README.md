ac68c5a93481d192f82897c55909ce324c00a579
wu <wu@.com>
2025-04-01 00:13:18 +0800

    jimdo/django-prometheus-metrics/action

--- README.md
+++ README.md
@@ -1,12 +1,68 @@
+# django-prometheus-metrics
 
-Ampoule - a process pool for Twisted, based on AMP
-==================================================
+Export Django monitoring metrics for Prometheus consumption.
 
-Ampoule is a process pool written on top of
-[Twisted](https://twistedmatrix.com/). Its name comes from the use of
-[AMP](https://amp-protocol.net/) as the default communication protocol between
-the pool and all its children.
+## Requirements
 
-It provides an API very close to that of the built-in Twisted `ThreadPool`. As
-an helper function it also provides a `deferToAMPProcess` function that creates
-the `ProcessPool` and submits jobs to it.
+- Django: `2.2`
+- Python `3.6`, `3.8`
+
+## Installation
+
+Install the package via pip:
+
+```
+pip install django-prometheus-metrics
+```
+
+Add it to your apps:
+
+```
+INSTALLED_APPS = (
+    # ...
+    'django_prometheus_metrics',
+    # ...
+)
+```
+
+Register the middleware:
+
+```
+MIDDLEWARE_CLASSES = (
+    'django_prometheus_metrics.middleware.PrometheusBeforeMiddleware',
+    # All your other middlewares go here
+    # ...
+)
+```
+
+Add the urls:
+
+```
+urlpatterns = [
+    # ...
+    url('', include('django_prometheus_metrics.urls')),
+    # ...
+]
+```
+
+## Exported metrics
+
+| Name                                 | Type      | Labels                          | Description                           |
+| ------------------------------------ | --------- | ------------------------------- | ------------------------------------- |
+| django_http_requests_total           | Counter   | `status_code`, `method`, `view` | Total count of requests               |
+| django_http_requests_latency_seconds | Histogram | `status_code`, `method`, `view` | Histogram of requests processing time |
+| django_model_inserts_total           | Counter   | `model`                         | Number of inserts on a certain model  |
+| django_model_updates_total           | Counter   | `model`                         | Number of updates on a certain model  |
+| django_model_deletes_total           | Counter   | `model`                         | Number of deletes on a certain model  |
+
+### Exproting model metrics
+
+In order to monitor a certain model your model has to inherit from the `MetricsModelMixin` in addition to the django model class:
+
+```
+from django_prometheus_metrics.models import MetricsModelMixin
+
+class Car(MetricsModelMixin('car'), models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    # ...
+```
--- setup.py
+++ setup.py
@@ -1,49 +1,36 @@
-#!/usr/bin/env python
-# -*- test-case-name: ampoule -*-
-
-# Copyright (c) 2008 Valentino Volonghi.
-# See LICENSE for details.
-
-"""
-Distutils/Setuptools installer for AMPoule.
-"""
-
-from setuptools import setup
-
-install_requires = ["Twisted[tls]>=17"]
-
-description = """A process pool built on Twisted and AMP."""
-long_description = open('README.md').read()
+from setuptools import setup, find_packages
 
 setup(
-    name = "ampoule",
-    author = "Valentino Volonghi",
-    author_email = "dialtone@gmail.com",
-    maintainer = "Glyph Lefkowitz",
-    maintainer_email = "glyph@twistedmatrix.com",
-    description = description,
-    description_content_type='text/markdown',
-    long_description = long_description,
-    long_description_content_type='text/markdown',
-    license = "MIT License",
-    install_requires=install_requires + ['incremental'],
-    url="https://github.com/glyph/ampoule",
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
+    name='django-prometheus-metrics',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT License',
+    description='Monitoring middleware for prometheus.',
+    long_description="""Django-Prometheus-Metrics
+This library lets you expose metrics in your django application for Prometheus consumption.
+See https://github.com/Jimdo/django-prometheus-metrics
+""",
+    url='https://github.com/Jimdo/django-prometheus-metrics',
+    author='Jimdo GmbH',
+    author_email='platformteam@jimdo.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Topic :: System',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    packages=["ampoule", "ampoule.test"],
-    package_data={'twisted': ['plugins/ampoule_plugin.py']},
-    use_incremental=True,
-    setup_requires=['incremental'],
-    include_package_data = True,
-    zip_safe=False
+    install_requires=[
+        "prometheus_client>=0.0.21",
+    ],
+    test_suite='runtests.runtests',
 )
