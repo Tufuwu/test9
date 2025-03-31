00d421b12ee9d3af01ac80e8d70ae6f66aa7c750
wu <wu@.com>
2025-04-01 00:08:23 +0800

    pytest-dev/pytest-stress/action

--- setup.py
+++ setup.py
@@ -1,40 +1,50 @@
-import random_word
-import setuptools
+#!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import codecs
+import os
 
-setuptools.setup(
-    name=random_word.__name__,
-    version=random_word.__version__,
-    author=random_word.__author__,
-    author_email="hi@vaibhavsingh97.com",
-    description="This is a simple python package to generate random english words",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+setup(
+    name="pytest-stress",
+    version="1.0.1",
+    author="Imran Mumtaz",
+    author_email="iomumtaz@gmail.com",
+    maintainer="Imran Mumtaz",
+    maintainer_email="iomumtaz@gmail.com",
     license="MIT",
-    keywords="package random words word of the day random word generator",
-    url="https://github.com/vaibhavsingh97/random-word",
-    packages=setuptools.find_packages(),
-    classifiers=(
+    url="https://pypi.org/project/pytest-stress",
+    description="A Pytest plugin that allows you to loop tests for a user defined amount of time.",
+    long_description=read("README.rst"),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    project_urls={
+        'Homepage': 'https://github.com/pytest-dev/pytest-stress',
+        'Source': 'https://github.com/pytest-dev/pytest-stress',
+        'Tracker': 'https://github.com/pytest-dev/pytest-stress/issues',
+    },
+    python_requires=">=3.6",
+    install_requires=["pytest>=3.6.0"],
+    entry_points={"pytest11": ["pytest_stress = pytest_stress.pytest_stress"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
-    ),
-    test_suite="nose.collector",
-    tests_require=["nose"],
-    install_requires=["requests", "nose"],
-    include_package_data=True,
-    zip_safe=False,
+        "License :: OSI Approved :: MIT License",
+    ],
 )
