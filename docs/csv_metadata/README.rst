8aa63633b0e1360716c0517f3b608b282c090e60
wu <wu@.com>
2025-04-01 01:39:17 +0800

    jealous/stockstats/action

--- docs/csv_metadata/README.rst
+++
@@ -1,4 +0,0 @@
-
-.. This is a added to avoid 404s.
-
-See the `index page <index.html>`__ to learn about CSV metadata workflow.
--- setup.py
+++ setup.py
@@ -1,75 +1,97 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import find_packages
+# coding=utf-8
+# Copyright (c) 2016, Cedric Zhuang
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of disclaimer nor the names of its contributors may
+#       be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from setuptools import setup
+import io
+import re
+import os
 
-import ricecooker
+__author__ = 'Cedric Zhuang'
 
 
-readme = open('README.md').read()
+def version():
+    desc = get_long_description()
+    ret = re.findall(r'VERSION: (.*)', desc)[0]
+    return ret.strip()
 
-with open('docs/history.rst') as history_file:
-    history = history_file.read()
 
-requirements = [
-    "pytest>=3.0.2",
-    "requests>=2.11.1",
-    "le_utils>=0.1.26",
-    "validators",                             # TODO: check if this is necessary
-    "requests_file",
-    "beautifulsoup4>=4.6.3,<4.9.0",   # pinned to match versions in le-pycaption
-    "pressurecooker>=0.0.30",
-    "selenium==3.0.1",
-    "youtube-dl>=2020.6.16.1",
-    "html5lib",
-    "cachecontrol==0.12.0",
-    "lockfile==0.12.2",                       # TODO: check if this is necessary
-    "css-html-js-minify==2.2.2",
-    "mock==2.0.0",
-    "pypdf2>=1.26.0",
-    "dictdiffer>=0.8.0",
-    "Pillow==5.4.1",
-    "colorlog>=4.1.0,<4.2",
-    "PyYAML>=5.3.1",
-    "Jinja2>=2.10"
-]
+def here(filename=None):
+    ret = os.path.abspath(os.path.dirname(__file__))
+    if filename is not None:
+        ret = os.path.join(ret, filename)
+    return ret
 
-test_requirements = [
-    # TODO: put package test requirements here
-]
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n\n')
+    buf = []
+    for filename in filenames:
+        with io.open(here(filename), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+def read_requirements(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
+
+
+def get_long_description():
+    filename = 'README.md'
+    return read(filename)
+
 
 setup(
-    name='ricecooker',
-    version=ricecooker.__version__,
-    description="API for adding content to the Kolibri content curation server",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
-    author="Learning Equality",
-    author_email='dev@learningequality.org',
-    url='https://github.com/learningequality/ricecooker',
-    packages=find_packages(),
-    package_dir={'ricecooker':'ricecooker'},
-    entry_points = {
-        'console_scripts': [
-            'corrections = ricecooker.utils.corrections:correctionsmain',
-            'jiro = ricecooker.cli:main'
-        ],
-    },
-    include_package_data=True,
-    install_requires=requirements,
-    license="MIT license",
-    zip_safe=False,
-    keywords='ricecooker',
+    name="stockstats",
+    version=version(),
+    author="Cedric Zhuang",
+    author_email="jealous@163.com",
+    description="DataFrame with inline stock statistics support.",
+    license="BSD",
+    keywords="stock statistics indicator",
+    url="https://github.com/jealous/stockstats",
+    py_modules=['stockstats'],
+    platforms=['any'],
+    long_description=get_long_description(),
     classifiers=[
-        'Intended Audience :: Developers',
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Natural Language :: English',
-        'Topic :: Education',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    install_requires=read_requirements('requirements.txt'),
+    tests_require=read_requirements('test-requirements.txt'),
+    long_description_content_type='text/markdown',
 )
