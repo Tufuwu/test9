8e55480eb13b1c18006366e2a0e3141d60894bb8
wu <wu@.com>
2025-04-01 00:01:43 +0800

    brean/python-pathfinding/action

--- dedupe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Deduplicate lines without sorting
-
-# (`sort -u $1` dedupes and sorts and is slower than awk)
-
-# Usage: dedupe.sh has_dupe_lines.txt > no_duplicate_lines.txt
-
-awk '!a[$0]++' $1
--- tweetables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Output only lines 140 or shorter
-
-# Usage: tweetables.sh short_and_long_lines.txt > only_short.txt
-
-awk 'length($0)<=140' $1
