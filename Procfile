2120c5440d0888794bc9566122f1d517e94fe4ce
wu <wu@.com>
2025-04-01 04:34:33 +0800

    rare-technologies/gensim/action

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py collectstatic --noinput --clear && python manage.py makemigrations && python manage.py migrate
-web: gunicorn tcf_core.wsgi --log-level debug
--- tcf_website/apps.py
+++
@@ -1,8 +0,0 @@
-"""TCF Django Web Application."""
-
-from django.apps import AppConfig
-
-
-class TcfWebsiteConfig(AppConfig):
-    """TCF Django Web Application Configuration."""
-    name = 'tcf_website'
