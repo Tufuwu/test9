8d545d98835e31c38314d974a4f4207ab321e913
wu <wu@.com>
2025-04-01 02:34:15 +0800

    barrust/mediawiki/action

--- early_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Execute light tests which can be called before setting up environment to save time and resources.
-cd src/
-python -m unittest tests.env.test_config
--- src/backend/utils.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def caption_tokenize(caption):
-    caption = re.sub('[^a-zA-Z]+', ' ', caption.encode('utf-8')).lower()
-    caption = caption.split()
-    return caption
