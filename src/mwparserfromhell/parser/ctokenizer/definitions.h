6d5479af9a29451ee8835f9dbeffc39eb2c975f0
wu <wu@.com>
2025-04-01 03:32:59 +0800

    earwig/mwparserfromhell/action

--- src/mwparserfromhell/parser/ctokenizer/definitions.h
+++ src/mwparserfromhell/parser/ctokenizer/definitions.h
@@ -1,8 +1,9 @@
-Copyright (C) 2012-2023 Ben Kurtovic <ben.kurtovic@gmail.com>
+/*
+Copyright (C) 2012-2016 Ben Kurtovic <ben.kurtovic@gmail.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights to
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
 use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
 of the Software, and to permit persons to whom the Software is furnished to do
 so, subject to the following conditions:
@@ -17,3 +18,21 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+#pragma once
+
+#include "common.h"
+
+/* This file should be kept up to date with mwparserfromhell/definitions.py. */
+
+/* Functions */
+
+int is_parsable(PyObject *);
+int is_single(PyObject *);
+int is_single_only(PyObject *);
+int is_scheme(PyObject *, int);
+
+/* Macros */
+
+#define GET_HTML_TAG(markup) (markup == ':' ? "dd" : markup == ';' ? "dt" : "li")
