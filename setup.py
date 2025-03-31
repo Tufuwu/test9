31bcff3bb2aa693b25bc704f1ae649acf5b3febc
wu <wu@.com>
2025-04-01 01:51:38 +0800

    mgaitan/sphinxcontrib-mermaid/action

--- setup.py
+++ setup.py
@@ -1,131 +1,64 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-###############################################################################
-#                                                                             #
-# Peekaboo Extended Email Attachment Behavior Observation Owl                 #
-#                                                                             #
-# setup.py                                                                    #
-###############################################################################
-#                                                                             #
-# Copyright (C) 2016-2022 science + computing ag                              #
-#                                                                             #
-# This program is free software: you can redistribute it and/or modify        #
-# it under the terms of the GNU General Public License as published by        #
-# the Free Software Foundation, either version 3 of the License, or (at       #
-# your option) any later version.                                             #
-#                                                                             #
-# This program is distributed in the hope that it will be useful, but         #
-# WITHOUT ANY WARRANTY; without even the implied warranty of                  #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           #
-# General Public License for more details.                                    #
-#                                                                             #
-# You should have received a copy of the GNU General Public License           #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
-#                                                                             #
-###############################################################################
-
-
-from codecs import open
-from os import path
-from sys import path as pythonpath
+import io
 from setuptools import setup, find_packages
 
-# Add peekaboo to PYTHONPATH
-pythonpath.append(path.dirname(path.dirname(path.abspath(__file__))))
-from peekaboo import __version__, __author__, __license__, __description__
+readme = io.open('README.rst', encoding="utf-8").read()
+changes = io.open('CHANGELOG.rst', encoding="utf-8").read()
+version = '0.7.1'
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def long_description():
+    """
+    return readme + changes, removing directive blocks that are only valid in the context
+    of sphinx doc"""
 
-# get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+    def remove_block(text, token, margin=0):
+        input_lines = text.splitlines()
+        for i, l in enumerate(input_lines):
+            if l.startswith(token):
+                break
+        start = i
+        end = input_lines.index("", start + margin)
+        return "\n".join(input_lines[:start] + input_lines[end:])
 
-install_requires = []
-for req in all_reqs:
-    # for git repo references, extract the egg/package name from the end and
-    # prepend it with @ as per pip Github issue 3939
-    if 'git+' in req:
-        egg = req.split('=')[-1]
-        req = "%s @ %s" % (egg, req)
+    readme_ = remove_block(readme, ".. mermaid::", margin=2)
+    readme_ = remove_block(readme_, ".. autoclasstree::")
+    readme_ = remove_block(readme_, ".. autoclasstree::")
+    readme_ = remove_block(readme_, ".. versionchanged::")
+    return "{}\n\n{}".format(readme_, changes)
 
-    install_requires.append(req.strip())
 
 setup(
-    name='PeekabooAV',
-    version=__version__,
-    description=__description__,
-    long_description=long_description,
-    url='https://github.com/scVENUS/PeekabooAV.git',
-    download_url='https://github.com/scVENUS/PeekabooAV/archive/master.zip',
-    license=__license__,
+    name='sphinxcontrib-mermaid',
+    version=version,
+    url='https://github.com/mgaitan/sphinxcontrib-mermaid',
+    download_url='https://pypi.python.org/pypi/sphinxcontrib-mermaid',
+    license='BSD',
+    author=u'Martín Gaitán',
+    author_email='gaitan@gmail.com',
+    description='Mermaid diagrams in yours Sphinx powered docs',
+    long_description=long_description(),
     classifiers=[
-      'Development Status :: 4 - Beta',
-      'Operating System :: POSIX',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 3',
-      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-      'Natural Language :: English',
-      'Natural Language :: German',
-      'Topic :: Communications :: Email :: Filters',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
     ],
-    keywords='Cuckoo, Amavis',
-    packages=find_packages(exclude=['docs', 'tests*']),
+    platforms='any',
+    packages=find_packages(),
     include_package_data=True,
-    # package_files augments MANIFEST.in in what is packaged into a
-    # distribution. Files to add must be inside a package. Thus files in the
-    # root of our source directory cannot be packaged with this. Files inside
-    # packages will stay there, totally obscured to the user. Meant for default
-    # configuration or other package-internal data.
-    #  package_files=[...],
-    #
-    # data_files is another way to augment what is installed from the
-    # distribution. Allows paths outside packages for both sources and targets.
-    # Absolute paths are strongly discouraged because they exhibit confusing if
-    # not downright broken behaviour. Relative paths are added to the
-    # distribution and then installed into
-    # site-packages/<package>-<ver>.egg/<relative path> (setup.py) or <python
-    # prefix>/<relative path> (pip). The latter works well with venvs and
-    # distribution-provided pip (e.g. /usr/local/<relative path>).
-    # We go this route for providing sample files because using setup.py
-    # directly is discouraged anyway and "only" tucks the files away in the egg
-    # directory, providing the most consistent option.
-    data_files=[
-        ("share/doc/peekaboo", [
-            "README.md",
-            "CHANGELOG.md",
-            "peekaboo.conf.sample",
-            "ruleset.conf.sample",
-            "analyzers.conf.sample",
-        ]),
-        ("share/doc/peekaboo/systemd", [
-            "systemd/peekaboo.service",
-        ]),
-        ("share/doc/peekaboo/amavis", [
-            "amavis/10-ask_peekaboo",
-        ]),
-    ],
-    # overriding the whole install_data command allows for arbitrary
-    # installation mechanics but does not solve the problem of adding files to
-    # a binary distribution (e.g. wheel, which pip uses internally always) in
-    # such a way that they will later be put at the correct location. Thus they
-    # would go around pip entirely, be missing from any actual wheel
-    # distribution package, pollute the system directly and not be removed upon
-    # uninstall or upgrade.
-    #  cmdclass={
-    #      'install_data': OffsetDataInstall,
-    #  },
-    author=__author__,
-    python_requires='>=3.6',
-    install_requires=install_requires,
-    author_email='felix.bauer@atos.net',
-    entry_points={
-        'console_scripts': [
-            'peekaboo = peekaboo.daemon:run',
-            'peekaboo-util = peekaboo.cli:main',
-        ],
-    }
+    namespace_packages=['sphinxcontrib'],
 )
