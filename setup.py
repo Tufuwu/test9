c703cdd533625e9d8bc1f79f0733ca5559a212df
wu <wu@.com>
2025-04-01 00:10:01 +0800

    curiouslearner/django-phone-verify/action

--- setup.py
+++ setup.py
@@ -1,25 +1,52 @@
-from setuptools import find_packages
-from setuptools import setup
+import os
 
-with open('README.md', 'r') as readme:
-    long_description = readme.read()
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    author='amancevice',
-    author_email='smallweirdnum@gmail.com',
-    description='Interact with InfluxDB using SQLAlchemy-style syntax',
+    name="django-phone-verify",
+    version="2.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    license="GPLv3",
+    description="A Django app to support phone number verification using security code sent via SMS.",
+    long_description=README,
+    url="https://github.com/CuriousLearner/django-phone-verify",
+    author="Sanyam Khurana",
+    author_email="sanyam@sanyamkhurana.com",
     install_requires=[
-        'influxdb >= 5.0',
-        'pytz >= 2018.3',
-        'requests >= 2.20',
-        'six >= 1.11',
+        "django>=2.1.5",
+        "djangorestframework>=3.9.0",
+        "PyJWT>=1.7.1",
+        "python-dotenv>=0.10.0",
+        "phonenumbers>=8.10.2",
+        "django-phonenumber-field>=2.1.0",
+        "twilio>=6.21.0",
+        "nexmo>=2.4.0",
     ],
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    name='influxalchemy',
-    packages=find_packages(exclude=['tests']),
-    python_requires='>= 3.5',
-    setup_requires=['setuptools_scm'],
-    url='https://github.com/amancevice/influxalchemy',
-    use_scm_version=True,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
