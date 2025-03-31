a9e6fed0f3ef3a4ccf88f49d723e4a40e49d8af2
wu <wu@.com>
2025-04-01 06:16:42 +0800

    enjoy-digital/litepcie/action

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
 
-import versioneer
+from setuptools import setup
+from setuptools import find_packages
 
-setup(name = 'socs',
-      version = versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description = 'Simons Observatory Control System',
-      package_dir = {'socs': 'socs'},
-      packages = find_packages())
+setup(
+    name="litepcie",
+    description="Small footprint and configurable PCIe core",
+    author="Florent Kermarrec",
+    author_email="florent@enjoy-digital.fr",
+    url="http://enjoy-digital.fr",
+    download_url="https://github.com/enjoy-digital/litepcie",
+    test_suite="test",
+    license="BSD",
+    python_requires="~=3.6",
+    install_requires=["pyyaml"],
+    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "litepcie_gen=litepcie.gen:main",
+        ],
+    },
+)
