aea9445d1dbe321e447e032d1173f11323e8e061
wu <wu@.com>
2025-04-01 03:34:11 +0800

    enjoy-digital/litedram/action

--- django_test_app/main_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainAppConfig(AppConfig):
-    """Configuration for ``main_app``."""
-
-    name = 'main_app'
--- django_test_app/main_app/logic/pure/migrations.py
+++
@@ -1,9 +0,0 @@
-import typing
-
-if typing.TYPE_CHECKING:
-    from main_app.models import SomeItem  # noqa: WPS433
-
-
-def is_clean_item(instance: 'SomeItem') -> bool:
-    """Pure function that decides whether or not whitespace is in the model."""
-    return ' ' not in instance.string_field
