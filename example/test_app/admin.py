71feead2394e9277705655cfe82ecf67f53409f6
wu <wu@.com>
2025-04-01 05:19:25 +0800

    improbable-eng/perforce-buildkite-plugin/action

--- example/test_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import ContentType, Permission
-from django.contrib import admin
-
-admin.site.register(ContentType)
-admin.site.register(Permission)
--- permissions_auditor/__init__.py
+++
@@ -1,6 +0,0 @@
-"Permissions auditing for Django."
-
-__version__ = '1.0.1'
-
-
-default_app_config = 'permissions_auditor.apps.PermissionsAuditorConfig'
