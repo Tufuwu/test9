0d27d5f403aa110fda19df63a1317e7ce4653c82
wu <wu@.com>
2025-04-01 03:18:40 +0800

    voc/plugin.video.media-ccc-de/action

---
+++ resources/lib/test_gui.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from __future__ import print_function, division, absolute_import
+
+from . import gui
+
+
+def test_err(recwarn):
+    gui.err("Some fatal error")
+    assert len(recwarn) == 1
+    w = recwarn.pop()
+    assert issubclass(w.category, UserWarning)
+    assert str(w.message) == "ERR: Some fatal error"
+
+
+def test_info(recwarn):
+    gui.info("Some information message")
+    assert len(recwarn) == 1
+    w = recwarn.pop()
+    assert issubclass(w.category, UserWarning)
+    assert str(w.message) == "INFO: Some information message"
