27ff0894b913f1b7e4b6ca9347ede8dd3fbf544f
wu <wu@.com>
2025-04-01 06:23:18 +0800

    sphinx-contrib/apidoc/action

--- setup.py
+++ setup.py
@@ -1,26 +1,9 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-setup(
-    name='aiochan',
-    version='0.2.6',
-    packages=find_packages(),
-    platforms='any',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    license='Apache',
-    python_requires='>=3.5.3',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-asyncio'],
-    author='Ziyang Hu',
-    author_email='hu.ziyang@cantab.net',
-    description='CSP-style concurrency for Python',
-    url='https://github.com/zh217/aiochan',
-    project_urls={},
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+import setuptools
+
+
+setuptools.setup(
+    setup_requires=['pbr>=4.0'],
+    pbr=True,
 )
